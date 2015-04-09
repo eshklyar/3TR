1c930ec4a8faa2ca6f33bfb2838c3136313d73e6
eshklyar <eshklyar@gmail.com>
2015-04-09 10:08:34 -0500

    somewhat working copy

--- 3TR/ViewController.m
+++ 3TR/ViewController.m
@@ -21,7 +21,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *Label9;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property NSArray *myLabels;
-@property NSMutableArray *tempLabels;
+@property NSMutableArray *remainingLabelsInGame;
 @property BOOL turn;
 @property NSInteger myLabelsIndex;
 @property NSMutableArray *gameMatrix;
@@ -29,7 +29,7 @@
 @property Player *playerOne;
 @property Player *playerTwo;
 
-@property NSArray *winningSet;
+@property NSMutableArray *winningSets;
 
 
 //@property NSString *matrixStrings[3][3];
@@ -40,20 +40,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.winningSet = @[
-                                            @[ @1, @2, @3],
-                                            @[ @4, @5, @6],
-                                            @[ @7, @8, @9],
 
+//    load 8 winning combinations into winningSets array with arrays, each array corespond to a a winning row/colum/diagonal line
+    self.winningSets = @[
+                                            @[ @0, @1, @2],
+                                            @[ @3, @4, @5],
+                                            @[ @6, @7, @8],
+
+                                            @[ @0, @3, @6],
                                             @[ @1, @4, @7],
                                             @[ @2, @5, @8],
-                                            @[ @1, @2, @3],
 
-                                            @[ @4, @5, @6],
-                                            @[ @7, @8, @9],
+                                            @[ @0, @4, @8],
+                                            @[ @2, @4, @6],
 
                                             ];
-    self.turnCount=0;
+
 
 //    self.matrix = @[
 //                    @[ @"", @"", @""],
@@ -63,53 +65,48 @@
 
 //    self.myLabelsIndex = 0;
     self.myLabels = @[self.Label1, self.Label2, self.Label3, self.Label4, self.Label5, self.Label6,self.Label7, self.Label8, self.Label9];
-    self.tempLabels = [NSMutableArray arrayWithArray:self.myLabels];
+    self.remainingLabelsInGame = [NSMutableArray arrayWithArray:self.myLabels];
 
-
+    self.turnCount=0;
     self.turn = TRUE;
+
     [self setwhichPlayerLabel:self.turn];
 
     [self setLabelsNewGame];
 
 
-
+//init original matrix with bool NO values
     self.gameMatrix = [NSMutableArray new];
-
-    for (int i=0; i<9; i++) {
-//        [self.tempMatrix addObject:[NSNumber numberWithBool:NO]];
-        [self.gameMatrix addObject:@NO];
-//        NSLog(@"added to matrix");
-//        NSLog(@"matrix inited %@", self.gameMatrix[0]);
-
-
-    }
-
-
-//    NSLog(@"this is first in matrix, %@", self.gameMatrix[0]);
-
+    [self matrixWithBoolNo:self.gameMatrix];
 
 #pragma Player
     //created two players and inited array property with bool faulse values
-
     self.playerOne = [Player new];
+    self.playerOne.name =@"playerOne";
 //    need to figure out how to init with bool in Player Class
     self.playerOne.matrix = [NSMutableArray arrayWithArray: self.gameMatrix];
-//    Player *playerOne = [[Player alloc] initWithMatrix: (NSMutableArray) *myMatrix];
 
-//    Player *playerTwo = [Player new];
     self.playerTwo = [Player new];
+    self.playerTwo.name =@"playerTwo";
     self.playerTwo.matrix = [NSMutableArray arrayWithArray: self.gameMatrix];
-//     NSLog(@"this is a game matrix 5 %@", self.gameMatrix[5]);
-
-//    NSLog(@"this is a matrix 5 %@", self.playerOne.matrix[5]);
-
 }
+
+//init the gameMatrix
+-(NSMutableArray*)matrixWithBoolNo:(NSMutableArray*)array{
+    for (int i=0; i<9; i++) {
+//      [self.tempMatrix addObject:[NSNumber numberWithBool:NO]];
+        [array addObject:@NO];
+    }
+    NSLog(@"matrixwithboolNO %@", array);
+    return array;
+}
+
 
 //set labels background color
 -(void)setLabelsNewGame {
-    for (UILabel *tempLabel in self.myLabels) {
-            tempLabel.text =@"";
-            tempLabel.backgroundColor =[UIColor grayColor];
+    for (UILabel *label in self.myLabels) {
+            label.text =@"";
+            label.backgroundColor =[UIColor grayColor];
         }
 }
 
@@ -126,21 +123,17 @@
 
 //method to detect user tapping on the screen
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)gesture {
-
 //    get the point of tap from gesture recognizer
     CGPoint point = [gesture locationInView:self.view];
-
 //    find label using findLabelUsingPoint method passing point as argument
     UILabel* searchLabel = [self findLabelUsingPoint:point];
-
 //    check player turn and set label accordingly
     if (!self.turn && searchLabel) {
         searchLabel.textColor =[UIColor blueColor];
         searchLabel.text =@"X";
+        NSLog(@"my myLabelsIndex is %ld", self.myLabelsIndex);
+        
         [self changeMatrixValueForPlayer:self.playerOne atIndex:self.myLabelsIndex];
-
-//testing
-        [self comaprePlayerMAtrix:self.playerOne.matrix];
 
         NSLog(@"player index mylabelsIndex %ld", (long)self.myLabelsIndex);
 
@@ -149,42 +142,39 @@
 //        self.turn = FALSE;
         [self setwhichPlayerLabel:self.turn];
     }
-    else
+    else if (self.turn && searchLabel)
     {
         searchLabel.textColor =[UIColor redColor];
         searchLabel.text =@"O";
         [self changeMatrixValueForPlayer:self.playerTwo atIndex:self.myLabelsIndex];
-         NSLog(@"player ixdex value 2 %@", self.playerTwo.matrix[self.myLabelsIndex]);
-         NSLog(@"player index hello  %ld", (long)self.myLabelsIndex);
+
+        NSLog(@"player ixdex value 2 %@", self.playerTwo.matrix[self.myLabelsIndex]);
+        NSLog(@"player index hello  %ld", (long)self.myLabelsIndex);
 
 
 //        self.turn = TRUE;
         [self setwhichPlayerLabel:self.turn];
     }
 }
-//return a selected label
-//this method is called from onLabelTapped()
-- (UILabel*) findLabelUsingPoint: (CGPoint)point  {
-//    NSInteger i=0;
 
-    for (UILabel* tempLabel in self.tempLabels)
+//this method is called from onLabelTapped()
+//return a selected label
+//sets myLabelsIndex - index of a selected label
+//removes selected label from the list
+//increments turn count and changes turn
+
+- (UILabel*) findLabelUsingPoint: (CGPoint)point  {
+    for (UILabel* tempLabel in self.remainingLabelsInGame)
     {
         if(CGRectContainsPoint(tempLabel.frame, point))
         {
-            //get and index of the selected label in myLabels array
-            self.myLabelsIndex=[self getMyLabelsIndex:tempLabel];
-
-//            NSLog(@"tempLabels %lu", (unsigned long)self.tempLabels.count);
-
+//get and index of the selected label in myLabels array
+            self.myLabelsIndex =[self getMyLabelsIndex:tempLabel];
             NSLog(@"myLabelsIndex inside findLabelUsingPoint is %ld", (long)self.myLabelsIndex);
 
-//            NSLog(@"index is %ld",(long)self.myLabelsIndex );
-////            [self cellMatrix:i];
-//            NSArray *cellFromMatrix =[self findCellInMatrixFromIndexInArray:self.myLabelsIndex];
-//            NSLog(@"this is row and colum %@, %@", cellFromMatrix[0], cellFromMatrix[1]);
-////            [self setLabelInMatrix:celFromMatrix];
-            [self.tempLabels removeObjectIdenticalTo:tempLabel];
-            [self SetTheGameEnd:self.turnCount];
+//remove marked label from the array
+            [self.remainingLabelsInGame removeObjectIdenticalTo:tempLabel];
+//            [self SetTheGameEnd:self.turnCount];
             self.turnCount++;
             if (self.turn) {
                 self.turn = FALSE;
@@ -192,17 +182,117 @@
             else{
                 self.turn = TRUE;
             }
-
-            return tempLabel;
+        return tempLabel;
         }
-
-//        [self.tempLabels removeObjectIdenticalTo:tempLabel];
-
-//        NSLog(@"tempLabels %lu", (unsigned long)self.tempLabels.count);
-//        i++;
     }
     return nil;
 }
+-(void)SetTheGameEnd: (NSInteger)count{
+
+    [self.gameMatrix replaceObjectAtIndex:count withObject:@YES];
+
+    if (count >= 8) {
+//    if ([self.gameMatrix containsObject:@NO]) {
+
+        NSLog(@"game over");
+    }
+
+}
+
+//get an idex of corresponding label in the array
+-(NSInteger)getMyLabelsIndex: (UILabel*)label{
+for (int i=0; i<9; i++)
+    {
+//    if ([self.myLabels containsObject:label])
+        if ([self.myLabels[i] isEqual:label]) {
+             NSLog(@"the getMyLabelsIndex index is %d", i);
+            return i;
+            NSLog(@"getMyLabelsIndex is %d",i);
+        }
+    {
+//        self.myLabelsIndex = i;
+//        NSLog(@"the index is %d", i);
+//        return i;
+    }
+}
+    return nil;
+}
+
+
+
+
+
+-(void)changeMatrixValueForPlayer:(Player*) player atIndex:(NSInteger) index{
+//    player.matrix[index] = @YES;
+    NSLog(@"changeMatrixValueForPlayer 've been called");
+    [player.matrix replaceObjectAtIndex:index withObject:@YES];
+    [self comparePlayerMatrix:self.winningSets with: player];
+
+}
+
+-(void)comparePlayerMatrix: (NSMutableArray*)winingArray with: (Player*) player
+{
+    NSLog(@"my player %@", player);
+
+    //go thru each array in the array with array of winningSet
+    for (int j=0; j<8; j++)
+    {
+        NSMutableArray* jArray = [NSMutableArray arrayWithArray:winingArray[j]];
+
+        int threeInRow=0;
+
+        for (int i=0; i<3; i++)
+        {
+            NSNumber *num = [jArray objectAtIndex:i];
+            int position = [num intValue];
+
+            NSLog(@"[player.matrix[position] %@", player.matrix[position]);
+
+
+            //            NSLog(@"i is %d", i);
+            //            int position = [NSNumber numberWithInteger: [jArray objectAtIndex:i] ];
+            //            NSLog(@"bla %@", [jArray objectAtIndex:i] );
+
+            //            NSLog(@"position again is %d", position);
+            //            NSLog(@"jarray at i is %@", [jArray objectAtIndex:i]);
+            //            NSLog(@"position is %d", position);
+            //            NSLog(@"testing testing bool %s", [player.matrix objectAtIndex:i] ? "true" : "false");
+            //            NSLog(@"testing testing bool %@ %d", [player.matrix objectAtIndex:i], i);
+
+            //            NSLog(@"player at 2 %@", player.matrix[2]);
+
+            //            NSLog(@"position is %d", position);
+            //            NSLog(@"[player.matrix objectAtIndex:position] %@",[player.matrix objectAtIndex:position] );
+            if ([player.matrix[position] isEqualToNumber:[NSNumber numberWithBool:@YES]])
+
+                //            if ([[player.matrix objectAtIndex: [jArray objectAtIndex:i]] isEqualToNumber:[NSNumber numberWithBool:@YES]])
+            {
+                //                NSLog(@"win!!!!!!!%@ %ld", [player.matrix objectAtIndex:position],(long)position);
+                threeInRow++;
+                NSLog(@"incrementing 3inRow, %d", threeInRow);
+                
+            }
+            else{
+                NSLog(@"no match");
+                 NSLog(@"3inRow, %d", threeInRow);
+            }
+        }
+        if (threeInRow==3) {
+            NSLog(@"win!!!!!!!");
+//            self.whichPlayerLabel.text =
+        }
+    }
+}
+
+//-(void)comaprePlayerMAtrix: (NSMutableArray*) array{
+//    BOOL winning = @NO;
+//    if ( [[array objectAtIndex:0] isEqualToNumber:[NSNumber numberWithBool: @"YES"]] )
+//    {
+//        NSLog(@"YES");
+//    }
+
+
+//}
 //-(void)setLabelInMatrix:(NSArray*)coordinat{
 // NSLog(@"matrix got you");
 ////
@@ -242,49 +332,5 @@
 //    return cellArray;
 //
 //}
--(void)SetTheGameEnd: (NSInteger)count{
 
-    [self.gameMatrix replaceObjectAtIndex:count withObject:@YES];
-
-    if (count >= 8) {
-//    if ([self.gameMatrix containsObject:@NO]) {
-
-        NSLog(@"game over");
-    }
-
-}
-
-//get an idex of corresponding label in the array
--(NSInteger)getMyLabelsIndex: (UILabel*)label{
-for (int i=0; i<=8; i++)
-    {
-//    if ([self.myLabels containsObject:label])
-        if ([self.myLabels[i] isEqual:label]) {
-             NSLog(@"the getMyLabelsIndex index is %d", i);
-            return i;
-        }
-    {
-//        self.myLabelsIndex = i;
-//        NSLog(@"the index is %d", i);
-//        return i;
-    }
-}
-    return nil;
-}
-
--(void)changeMatrixValueForPlayer: (Player*) player atIndex:(NSInteger)index{
-    player.matrix[index] = @YES;
-    [player.matrix replaceObjectAtIndex:index withObject:@YES];
-//    NSLog(@"changeMatrixValueForPlayer 've been called");
-}
-
--(void)comaprePlayerMAtrix: (NSMutableArray*) array{
-    BOOL winning = @NO;
-    if ( [[array objectAtIndex:0] isEqualToNumber:[NSNumber numberWithBool: @"YES"]] )
-    {
-        NSLog(@"YES");
-    }
-
-
-}
 @end
